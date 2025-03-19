451dce4d8bb15e825c3f24d23e32b220c1c2dd22
Francisco <francdrigue@gmail.com>
2025-03-18 22:50:58 -0300

    create guestsRepository and guestFormViewModel

---
+++ app/src/main/java/com/example/convidados/repository/GuestRepository.kt
@@ -0,0 +1,15 @@
+package com.example.convidados.repository
+
+class GuestRepository private constructor(){
+    // Singleton
+    companion object {
+        private lateinit var repository: GuestRepository
+
+        fun getInstance(): GuestRepository {
+            if(!::repository.isInitialized) {
+                repository = GuestRepository()
+            }
+            return repository
+        }
+    }
+}
--- app/src/main/java/com/example/convidados/view/GuestFormActivity.kt
+++ app/src/main/java/com/example/convidados/view/GuestFormActivity.kt
@@ -3,17 +3,22 @@ package com.example.convidados.view
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.convidados.R
 import com.example.convidados.databinding.ActivityGuestFormBinding
+import com.example.convidados.viewmodel.GuestFormViewModel
 
 class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var guestViewModel: GuestFormViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGuestFormBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        guestViewModel = ViewModelProvider(this)[GuestFormViewModel::class.java]
 
         binding.buttonSave.setOnClickListener(this)
         binding.RadioPresent.isChecked = true
