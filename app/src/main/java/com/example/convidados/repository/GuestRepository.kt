6d123b15ec28c9a965f5e5af7480e2b5e3a6d037
Francisco <francdrigue@gmail.com>
2025-03-24 22:38:43 -0300

    create function update and function delete in GuestRepository

--- app/src/main/java/com/example/convidados/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/repository/GuestRepository.kt
@@ -5,17 +5,17 @@ import android.content.Context
 import com.example.convidados.infra.Const
 import com.example.convidados.model.GuestModel
 
-class GuestRepository private constructor(context: Context){
+class GuestRepository private constructor(context: Context) {
 
 
-    private var guestDataBase =  GuestDataBase(context)
+    private var guestDataBase = GuestDataBase(context)
 
     // Singleton
     companion object {
         private lateinit var repository: GuestRepository
 
         fun getInstance(context: Context): GuestRepository {
-            if(!::repository.isInitialized) {
+            if (!::repository.isInitialized) {
                 repository = GuestRepository(context)
             }
             return repository
@@ -23,20 +23,55 @@ class GuestRepository private constructor(context: Context){
     }
 
     fun insert(guest: GuestModel): Boolean {
-       return  try {
-             val db = guestDataBase.writableDatabase
+        return try {
+            val db = guestDataBase.writableDatabase
 
-             val presence = if (guest.presence) 1 else 0
+            val presence = if (guest.presence) 1 else 0
 
-             val values = ContentValues().apply {
-                 put(Const.KEY.COLUMN_NAME, guest.name)
-                 put(Const.KEY.COLUMN_PRESENCE, presence)
-             }
+            val values = ContentValues().apply {
+                put(Const.KEY.COLUMN_NAME, guest.name)
+                put(Const.KEY.COLUMN_PRESENCE, presence)
+            }
 
-             db.insert(Const.KEY.TABLE_NAME, null, values)
-             true
-         }catch (e: Exception) {
-             false
-         }
+            db.insert(Const.KEY.TABLE_NAME, null, values)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun update(guest: GuestModel): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val presence = if (guest.presence) 1 else 0
+
+            val values = ContentValues().apply {
+                put(Const.KEY.COLUMN_NAME, guest.name)
+                put(Const.KEY.COLUMN_PRESENCE, presence)
+            }
+
+            val clause = "${Const.KEY.COLUMN_ID} = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(Const.KEY.TABLE_NAME, values, clause, args)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun delete(id: Int): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val clause = "${Const.KEY.COLUMN_ID} = ?"
+            val agr = arrayOf(id.toString())
+
+            db.delete(Const.KEY.TABLE_NAME, clause, agr)
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 }
