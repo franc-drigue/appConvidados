e621e81de135d31318fc786a42c17f91169b3d86
Francisco <francdrigue@gmail.com>
2025-04-12 20:15:02 -0300

    adjustment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation ("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("com.mikepenz:iconics-core:5.3.1")
+    implementation ("com.mikepenz:google-material-typeface:4.0.0.3-kotlin@aar")
 }
--- app/src/main/java/com/example/convidados/view/viewholder/GuestViewHolder.kt
+++ app/src/main/java/com/example/convidados/view/viewholder/GuestViewHolder.kt
@@ -1,18 +1,29 @@
 package com.example.convidados.view.viewholder
 
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.convidados.databinding.RowGuestBinding
 import com.example.convidados.model.GuestModel
 
 // Esta é a classe que recebe o item na classe GuestsAdapter
 class GuestViewHolder(private val bind: RowGuestBinding) : RecyclerView.ViewHolder(bind.root) {
+    private val context: Context = bind.root.context
+    private val builder = AlertDialog.Builder(context)
 
     // passando o nome para o layout
     fun bind(guest: GuestModel, onDeleteClick: (Int) -> Unit) {
         bind.name.text = guest.name
 
         bind.iconDelete.setOnClickListener {
-            onDeleteClick(guest.id) // Passa o ID do convidado ao clicar
+            builder.setTitle("Deletar convidado")
+            builder.setMessage("Deseja deletar esse convidado?")
+            builder.setPositiveButton("Continuar") { _, _ ->
+                onDeleteClick(guest.id) // Passa o ID do convidado ao clicar
+            }
+            builder.setNegativeButton("Cancelar") {dialog, _ ->
+                dialog.dismiss()
+            }.create().show()
         }
     }
 }
